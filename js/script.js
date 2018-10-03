5f3ad7a406d6f33ddaf23bedb465e777c2c27813
lbergengruen <lbergengruen@correo.um.edu.uy>
2018-10-03 14:06:48 -0300

    Avance Jumbotron

--- js/script.js
+++ js/script.js
@@ -4,12 +4,15 @@ carousel();
 function carousel() {
     var i;
     var x = document.getElementsByClassName("mySlides");
+    var y = document.getElementsByClassName("mySign");
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none"; 
+      y[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
+    y[slideIndex-1].style.display = "block";
     setTimeout(carousel, 4000); // Change image every 2 seconds
 }
 
