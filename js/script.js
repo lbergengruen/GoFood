8e438b239860388de18db008180927927772629e
lbergengruen <lbergengruen@correo.um.edu.uy>
2018-10-03 13:11:31 -0300

    Fin clase

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+}
+
+/************************************* Mi codigo ********************************************/
 $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
