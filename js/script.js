21c55049fbdd0286ca5c931862b79a068f6475a4
lbergengruen <lbergengruen@correo.um.edu.uy>
2018-10-11 16:57:27 -0300

    ffd

--- js/script.js
+++ js/script.js
@@ -13,136 +13,131 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 (function (global) {
 
-var dc = {};
+  var dc = {};
 
-var homeHtml = "../snipets/home_snipet.html";
-var allCategoriesUrl = "#";
-var pedidoHtml = "../snipets/pedido_snipet.html";
-var calculadoraHtml = "../snipets/calculadora_snippet.html";
+  var homeHtml = "../snipets/home_snipet.html";
+  var pedidoHtml = "../snipets/pedido_snipet.html";
+  var calculadoraHtml = "../snipets/calculadora_snippet.html";
 
-var insertHtml = function (selector, html) {
-  var targetElem = document.querySelector(selector);
-  targetElem.innerHTML = html;
-};
+  var insertHtml = function (selector, html) {
+    var targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  };
 
-var showLoading = function (selector) {
-  var html = "<div class='text-center'>";
-  html += "<div class='loader'></div>";
-  insertHtml(selector, html);
-};
+  var showLoading = function (selector) {
+    var html = "<div class='text-center'>";
+    html += "<div class='loader'></div>";
+    insertHtml(selector, html);
+  };
 
-// Remove the class 'active' from home and switch to Menu button
-var switchHomeToActive = function () {
-  // Remove 'active' from home button
-  var classes = document.querySelector("#navMenuButton").className;
-  classes = classes.replace(new RegExp("active", "g"), "");
-  document.querySelector("#navMenuButton").className = classes;
-
-  classes = document.querySelector("#navCalculadoraButton").className;
-  classes = classes.replace(new RegExp("active", "g"), "");
-  document.querySelector("#navCalculadoraButton").className = classes;
-
-  // Add 'active' to menu button if not already there
-  classes = document.querySelector("#navHomeButton").className;
-  if (classes.indexOf("active") === -1) {
-    classes += " active";
-    document.querySelector("#navHomeButton").className = classes;
-  }
-};
-
-var switchMenuToActive = function () {
-  // Remove 'active' from home button
-  var classes = document.querySelector("#navHomeButton").className;
-  classes = classes.replace(new RegExp("active", "g"), "");
-  document.querySelector("#navHomeButton").className = classes;
-
-  classes = document.querySelector("#navCalculadoraButton").className;
-  classes = classes.replace(new RegExp("active", "g"), "");
-  document.querySelector("#navCalculadoraButton").className = classes;
-
-  // Add 'active' to menu button if not already there
-  classes = document.querySelector("#navMenuButton").className;
-  if (classes.indexOf("active") === -1) {
-    classes += " active";
+  // Remove the class 'active' from home and switch to Menu button
+  var switchHomeToActive = function () {
+    // Remove 'active' from home button
+    var classes = document.querySelector("#navMenuButton").className;
+    classes = classes.replace(new RegExp("active", "g"), "");
     document.querySelector("#navMenuButton").className = classes;
-  }
-};
 
-var switchCalculadoraToActive = function () {
-  var classes = document.querySelector("#navHomeButton").className;
-  classes = classes.replace(new RegExp("active", "g"), "");
-  document.querySelector("#navHomeButton").className = classes;
-
-  classes = document.querySelector("#navMenuButton").className;
-  classes = classes.replace(new RegExp("active", "g"), "");
-  document.querySelector("#navMenuButton").className = classes;
-
-  // Add 'active' to menu button if not already there
-  classes = document.querySelector("#navCalculadoraButton").className;
-  if (classes.indexOf("active") === -1) {
-    classes += " active";
+    classes = document.querySelector("#navCalculadoraButton").className;
+    classes = classes.replace(new RegExp("active", "g"), "");
     document.querySelector("#navCalculadoraButton").className = classes;
+
+    // Add 'active' to menu button if not already there
+    classes = document.querySelector("#navHomeButton").className;
+    if (classes.indexOf("active") === -1) {
+      classes += " active";
+      document.querySelector("#navHomeButton").className = classes;
+    }
+
+    document.getElementById("pedirBoton").addEventListener("click", function(){
+      buildAndShowPedidosHTML();
+    });
+  };
+
+  var switchMenuToActive = function () {
+    // Remove 'active' from home button
+    var classes = document.querySelector("#navHomeButton").className;
+    classes = classes.replace(new RegExp("active", "g"), "");
+    document.querySelector("#navHomeButton").className = classes;
+
+    classes = document.querySelector("#navCalculadoraButton").className;
+    classes = classes.replace(new RegExp("active", "g"), "");
+    document.querySelector("#navCalculadoraButton").className = classes;
+
+    // Add 'active' to menu button if not already there
+    classes = document.querySelector("#navMenuButton").className;
+    if (classes.indexOf("active") === -1) {
+      classes += " active";
+      document.querySelector("#navMenuButton").className = classes;
+    }
+  };
+
+  var switchCalculadoraToActive = function () {
+    var classes = document.querySelector("#navHomeButton").className;
+    classes = classes.replace(new RegExp("active", "g"), "");
+    document.querySelector("#navHomeButton").className = classes;
+
+    classes = document.querySelector("#navMenuButton").className;
+    classes = classes.replace(new RegExp("active", "g"), "");
+    document.querySelector("#navMenuButton").className = classes;
+
+    // Add 'active' to menu button if not already there
+    classes = document.querySelector("#navCalculadoraButton").className;
+    if (classes.indexOf("active") === -1) {
+      classes += " active";
+      document.querySelector("#navCalculadoraButton").className = classes;
+    }
+  };
+
+  // On page load (before images or CSS)
+  document.addEventListener("DOMContentLoaded", function (event) {
+    buildAndShowHomeHTML();
+  });
+
+  document.getElementById("navHomeButton").addEventListener("click", function(){
+    buildAndShowHomeHTML();
+  });
+
+  document.getElementById("navMenuButton").addEventListener("click", function(){
+    buildAndShowPedidosHTML();
+  });
+
+  document.getElementById("navCalculadoraButton").addEventListener("click", function(){
+    buildAndShowCalculadoraHTML();
+  });
+
+
+  function buildAndShowHomeHTML () {
+
+    $ajaxUtils.sendGetRequest(
+      homeHtml,
+      function (homeHtml) {
+        insertHtml("#main-content", homeHtml);
+        switchHomeToActive();
+      },
+      false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
   }
-};
-
-// On page load (before images or CSS)
-document.addEventListener("DOMContentLoaded", function (event) {
-  buildAndShowHomeHTML();
-});
-
-document.getElementById("navHomeButton").addEventListener("click", function(){
-  buildAndShowHomeHTML();
-});
-
-document.getElementById("navMenuButton").addEventListener("click", function(){
-  buildAndShowPedidosHTML();
-});
-
-document.getElementById("pedirBoton").addEventListener("click", function(){
-  buildAndShowPedidosHTML();
-});
-
-document.getElementsByClassName("calcBotton").addEventListener("click", function(){
-  buildAndShowCalculadoraHTML();
-});
-
-document.getElementById("navCalculadoraButton").addEventListener("click", function(){
-  buildAndShowCalculadoraHTML();
-});
 
 
-function buildAndShowHomeHTML () {
+  function buildAndShowPedidosHTML () {
+    $ajaxUtils.sendGetRequest(
+      pedidoHtml,
+      function (pedidoHtml) {
+        switchMenuToActive();
+        insertHtml("#main-content", pedidoHtml);
+      },
+      false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
+    
 
-  $ajaxUtils.sendGetRequest(
-    homeHtml,
-    function (homeHtml) {
-      switchHomeToActive();
-      insertHtml("#main-content", homeHtml);
-    },
-    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
-}
-
-
-function buildAndShowPedidosHTML () {
-  $ajaxUtils.sendGetRequest(
-    pedidoHtml,
-    function (pedidoHtml) {
-      switchMenuToActive();
-      insertHtml("#main-content", pedidoHtml);
-    },
-    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
-  
-
-}
+  }
 
 
-function buildAndShowCalculadoraHTML () {
-  switchCalculadoraToActive();
-  insertHtml("#main-content", calculadoraHtml);
-}
+  function buildAndShowCalculadoraHTML () {
+    switchCalculadoraToActive();
+    insertHtml("#main-content", calculadoraHtml);
+  }
 
 
-global.$dc = dc;
+  global.$dc = dc;
 
 })(window);
 
