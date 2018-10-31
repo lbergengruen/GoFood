746fdd524938e409daea35cb47be73db1f18b428
telematica <telematica@F202-LCZG.sam.um.edu.uy>
2018-10-31 13:10:43 -0300

    nada

--- js/script.js
+++ js/script.js
@@ -215,3 +215,18 @@ function carousel() {
     y[slideIndex-1].style.display = "block";
     setTimeout(carousel, 4000); // Change image every 2 seconds
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
